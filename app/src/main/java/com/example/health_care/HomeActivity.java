ead0f0b35d79b96454d87392578eb6a7e33bb421
Ankush-2030 <116619468+Ankush-2030@users.noreply.github.com>
2023-02-17 18:42:30 +0530

    UI Changes

--- app/src/main/java/com/example/health_care/HomeActivity.java
+++ app/src/main/java/com/example/health_care/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.health_care;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared-prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(),"Welcome!" + username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+
+
+        CardView findDoctor = findViewById(R.id.cardFindDoctor);
+        findDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity((new Intent(HomeActivity.this, FindDoctorActivity.class)));
+            }
+        });
+
+        CardView labtest = findViewById(R.id.cardLabTest);
+        labtest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, LabTestActivity.class));
+            }
+        });
     }
 }
